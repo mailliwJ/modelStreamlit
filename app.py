dadc769da3c873d0b738eb423e218ed639a9c81f
mailliwJ <167079494+mailliwJ@users.noreply.github.com>
2024-12-02 17:34:04 +0100

    sidebar

--- app.py
+++ app.py
@@ -1,31 +1,52 @@
 import streamlit as st
 import requests
 
-st.title('Rainfall Prediction App')
-st.markdown('Enter climatic paramters to predict rainfall in mm')
 
-pressure = st.number_input('Pressure (Pa)', min_value=0, max_value=200000)
-sunshine = st.number_input('Sunshine (hrs)', min_value=0, max_value=24)
-mean_temp = st.number_input('Mean Temperature (oC)', min_value=-50, max_value=50)
+BASE_URL = 'https://mailliwj.pythonanywhere.com/'
 
-if st.button('Predict Rainfall'):
-    api_url = 'https://mailliwj.pythonanywhere.com/predict'
-    payload = {
-        'pressure': pressure,
-        'sunshine': sunshine,
-        'mean_temp': mean_temp
-    }
+# Endpoints
+HOME_URL = f'{BASE_URL}'
+PREDICT_URL = f'{BASE_URL}/predict'
 
-    try:
-        response = requests.post(api_url, json=payload)
-        response.raise_for_status()
-        result = response.json()
-        prediction = result.get('Prediction', None)
+def main():
+    st.title('Rainfall Prediction Application')
+    st.sidebar.title('Navigation')
 
-        if prediction is not None:
-            st.success(f' The predicted rainfall is {prediction:.1f}mm')
-        else:
-            st.error('Could not retrieve a valid prediction from the API')
+    menu = st.sidebar.radio('Menu', ['Predict', 'Forecast', 'Update Data', 'Retrain'])
+    if menu == "Predict":
+        predict(PREDICT_URL)
+    elif menu == "Forecast":
+        st.write('Forecast feature coming soon')
+    elif menu == 'Update Data':
+        st.write('Update Data feature coming soon')
+    elif menu == 'Retrain':
+        st.write('Retrain feature coming soon')
 
-    except requests.exceptions.RequestException as e:
-        st.error(f'Error contacting the API: (e)')
+def predict(PREDICT_URL):
+    st.title('Rainfall Prediction App')
+    st.markdown('Enter climatic paramters to predict rainfall in mm')
+
+    pressure = st.number_input('Pressure (Pa)', min_value=0, max_value=200000)
+    sunshine = st.number_input('Sunshine (hrs)', min_value=0, max_value=24)
+    mean_temp = st.number_input('Mean Temperature (oC)', min_value=-50, max_value=50)
+
+    if st.button('Predict Rainfall'):
+        payload = {
+            'pressure': pressure,
+            'sunshine': sunshine,
+            'mean_temp': mean_temp
+        }
+
+        try:
+            response = requests.post(PREDICT_URL, json=payload)
+            response.raise_for_status()
+            result = response.json()
+            prediction = result.get('Prediction', None)
+
+            if prediction is not None:
+                st.success(f' The predicted rainfall is {prediction:.1f}mm')
+            else:
+                st.error('Could not retrieve a valid prediction from the API')
+
+        except requests.exceptions.RequestException as e:
+            st.error(f'Error contacting the API: (e)')
